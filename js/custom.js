aacbf8260c8cac0e1d57176707181894fae9e8bd
Anwar Tamimi <osama@gmail.com>
2022-10-19 01:41:08 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+/* Global $ */
+
+$(function(){
+
+    'use strict';
+    $('.info-list li').click(function(){
+        $(this).addClass('selected').siblings('li').removeClass('selected');
+
+        window.console.log($(this).data('class'));
+
+        $('.info-content div').hide();
+
+        $('.' + $(this).data('class')).fadeIn();
+
+    });
+});
